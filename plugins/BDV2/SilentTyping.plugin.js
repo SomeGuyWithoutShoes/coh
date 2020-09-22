0dab65c2752a3b4a8729b23bfad10862c0df8784
SomeGuyWithoutShoes <support.cihparg@live.fi>
2020-09-22 10:58:01 +0300

    Remove utils, replace old bdapi with new bdapi.

--- plugins/BDV2/SilentTyping.plugin.js
+++ plugins/BDV2/SilentTyping.plugin.js
@@ -1,43 +1,34 @@
 //META{"name":"SilentTyping"}*//
 class SilentTyping {constructor() {
-    
     const Export = new class $export {}
     const Plugin = new class $plugin {}
-    
     Plugin.loader = () => {
-        Plugin.modules = BDV2.WebpackModules.findByUniqueProperties(["startTyping"])
+        Plugin.modules = BdApi.findModuleByProps("startTyping")
         
         Object.assign(Export.default, {Export, Plugin})
-        Utils.log(`[${Export.getters.getName()}] Plugin initialized.`)
     }
     Plugin.starter = () => {
-        if (!Plugin.restore) Plugin.restore = Utils.monkeyPatch(Plugin.modules, "startTyping", {
+        if (!Plugin.restore) Plugin.restore = BdApi.monkeyPatch(Plugin.modules, "startTyping", {
             instead () {}
         })
-        Utils.log(`[${Export.getters.getName()}] Plugin enabled.`)
     }
     Plugin.stopper = () => {
         if (Plugin.restore) Plugin.restore()
-        Utils.log(`[${Export.getters.getName()}] Plugin disabled.`)
     }
-    
     Export.events = {
         load: Plugin.loader || (() => !0),
         unload: Plugin.unloader || (() => !0),
         start: Plugin.starter || (() => !0),
         stop: Plugin.stopper || (() => !0)
     }
-    
     Export.getters = {
         getName: () => "SilentTyping",
-        getDescription: () => "Silent typing.",
-        getVersion: () => "Version",
-        getAuthor: () => ":COH:",
-        getSettingsPanel: () => "Nothing to see here"
+        getDescription: () => !1,
+        getVersion: () => !1,
+        getAuthor: () => !1,
+        getSettingsPanel: !1
     }
-    
     Export.default = {}
-    
     return Object.assign(new class $module {}, Export.getters, Export.events, {default: Export.default})
     
 }}
