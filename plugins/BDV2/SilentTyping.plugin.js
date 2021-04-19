dc2db3592d6ccdbaf1a0025af45509766aa759aa
SomeGuyWithoutShoes <support.cihparg@live.fi>
2021-04-19 18:24:22 +0300

    export compliance

--- plugins/BDV2/SilentTyping.plugin.js
+++ plugins/BDV2/SilentTyping.plugin.js
@@ -1,34 +1,18 @@
-//META{"name":"SilentTyping"}*//
-class SilentTyping {constructor() {
-    const Export = new class $export {}
-    const Plugin = new class $plugin {}
-    Plugin.loader = () => {
-        Plugin.modules = BdApi.findModuleByProps("startTyping")
-        
-        Object.assign(Export.default, {Export, Plugin})
+/**
+  * @name SilentTyping
+  */
+module.exports = class {
+    load () {
+        this.hook = BdApi.findModuleByProps("startTyping")
     }
-    Plugin.starter = () => {
-        if (!Plugin.restore) Plugin.restore = BdApi.monkeyPatch(Plugin.modules, "startTyping", {
-            instead () {}
-        })
+    start () {
+        if (!this.restore)
+            this.restore = BdApi.monkeyPatch(this.hook, "startTyping", {
+                instead () {}
+            })
     }
-    Plugin.stopper = () => {
-        if (Plugin.restore) Plugin.restore()
+    stop () {
+        if (this.restore)
+            this.restore()
     }
-    Export.events = {
-        load: Plugin.loader || (() => !0),
-        unload: Plugin.unloader || (() => !0),
-        start: Plugin.starter || (() => !0),
-        stop: Plugin.stopper || (() => !0)
-    }
-    Export.getters = {
-        getName: () => "SilentTyping",
-        getDescription: () => !1,
-        getVersion: () => !1,
-        getAuthor: () => !1,
-        getSettingsPanel: !1
-    }
-    Export.default = {}
-    return Object.assign(new class $module {}, Export.getters, Export.events, {default: Export.default})
-    
-}}
+} 
